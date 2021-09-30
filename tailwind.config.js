0d7a304980491db03a85956fcbefb5c2b45c5c07
Andy <andytcodes@gmail.com>
2021-09-30 16:42:22 -0400

    Initial commit and landing page

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {}
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('daisyui')
+  ],
+
+  daisyui: {
+    styled: true,
+    themes: [
+      'halloween',
+      'dark',
+      'forest',
+      'synthwave'
+    ]
+  }
+}
