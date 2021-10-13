96aff4eda3c3bd7ee35f361fc8b98bcdc75c9d31
Andy <andytcodes@gmail.com>
2021-10-13 19:08:00 -0400

    switched forgot password page to light ui

--- src/components/ForgotPassword.js
+++ src/components/ForgotPassword.js
@@ -28,23 +28,23 @@ export default function ForgotPassword() {
 
   return (
     <>
-      <div>
-        <div class="modal-box App">
+      <div class="hero min-h-screen bg-base-content">
+        <div class="modal-box App bg-neutral-content">
           <h2 className="text-primary font-bold text-3xl">Password Reset</h2>     
           <form onSubmit={handleSubmit}>
             <div id="email" class="form-control">
               <label class="label">
-              <label class="label-text">Email</label><br/>
+              <label class="label-text text-black">Email</label><br/>
               </label>
-              <input class="input input-bordered input-primary mb-4" type="email" ref={emailRef} required />
-              {error && <alert variant="danger">{error}</alert>}
-              {message && <alert variant="success">{message}</alert>}     
+              <input class="input input-bordered input-primary mb-4 bg-neutral-content text-black" type="email" ref={emailRef} required />
+              {error && <alert variant="danger" class="text-error font-semibold">{error}</alert>}
+              {message && <alert variant="success" class="text-success font-semibold">{message}</alert>}     
             </div>
             <div class="modal-action">
               <button className="btn btn-primary" disabled={loading} type="submit">
                 Reset Password
               </button>
-              <a href="/" class="btn">Close</a>
+              <a href="/" class="btn btn-primary">Close</a>
             </div>
           </form>
           <div className="link text-primary mt-6">
